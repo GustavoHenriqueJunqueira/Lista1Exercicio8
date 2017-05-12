6c09232d26d7b3f545516e6506e93037f400f5b6
Gustavo Henrique f. Junqueira <gustavohenriquejunqueira@hotmail.com>
2017-05-12 09:16:57 -0300

    Lista1Exercicio8
    Faça o algoritmo (diagrama de blocos e português estruturado) para
    calcular o valor a ser pago pelo período de estacionamento do automóvel
    (PAG). O usuário entra com os seguintes dados: hora (HE) e minuto (ME)
    de entrada, hora (HS) e minuto (MS) de saída. Sabe-se que este
    estacionamento cobra R$ 4,00, mas calcula as frações de hora também. Por
    exemplo, se a pessoa ficar 1 hora e quinze minutos, pagará R$ 5,00 (R$
    4,00 pela hora e R$ 1,00 pelos quinze minutos).

---
+++ Lista1Exercicio8.java
@@ -0,0 +1,22 @@
+package lista1exercicio8;
+
+import java.util.Scanner;
+
+public class Lista1Exercicio8 {
+
+    public static void main(String[] args) {
+       
+        System.out.println("Programa para calcular o período de estacionamento do automóvel. ");
+        
+        float PAG, HE, ME, HS, MS;
+        
+        System.out.println("Digite a hora de permanencia: ");
+        Scanner teclado = new Scanner(System.in);
+        HE = teclado.nextFloat();
+        System.out.println("Digite os minutos caso haja:");
+        ME = teclado.nextFloat();
+        
+ 
+    }
+    
+}
